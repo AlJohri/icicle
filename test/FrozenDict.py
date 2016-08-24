2ee514c4383e35cf9d14b8bcc956bd0304e2392d
Pedro Cattori <pcattori@gmail.com>
2016-08-24 18:16:20 -0400

    basic FrozenDict tests

--- test/FrozenDict.py
+++ test/FrozenDict.py
@@ -3,7 +3,23 @@ import unittest
 from icicle import FrozenDict
 
 class FrozenDictTest(unittest.TestCase):
-  pass
+
+  def test_create_from_dict(self):
+    d = {'a': 1, 'b': 2}
+    fd = FrozenDict(d)
+    self.assertEquals(fd['a'], 1)
+    self.assertEquals(fd['b'], 2)
+
+  def test_convert_to_dict(self):
+    fd = FrozenDict(a=1, b=2)
+    d = dict(fd)
+    self.assertEquals(d, {'a': 1, 'b': 2})
+
+  def test_immutable(self):
+    fd = FrozenDict(a=1)
+    with self.assertRaises(TypeError) as context:
+      fd['a'] = 2
+    self.assertIn("'FrozenDict' object does not support item assignment", context.exception.message)
 
 if __name__ == '__main__':
   unittest.main()
