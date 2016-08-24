7d8baa08392fbee8efbc69dd5b367b71a206e7c3
Pedro Cattori <pcattori@gmail.com>
2016-08-23 20:00:46 -0400

    first implementations

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+  name='frozen',
+  version='0.1.0',
+  description='Python FrozenDict and DeepFreeze',
+  url='https://github.com/pcattori/frozen',
+  author='Pedro Cattori',
+  author_email='pcattori@gmail.com',
+  license='MIT',
+  packages=['frozen']
+)
