781e134f0e7ec6e4f058c1456777eb7cdc918120
Pedro Cattori <pcattori@gmail.com>
2016-09-24 01:14:28 -0400

    removed freeze from description

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
   name='icicle',
   version='0.2.1',
-  description='Python FrozenDict and DeepFreeze',
+  description='Python FrozenDict',
   url='https://github.com/pcattori/icicle',
   author='Pedro Cattori',
   author_email='pcattori@gmail.com',
